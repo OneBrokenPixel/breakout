d789729689c172459d8c154a61d389cb44683f2e
Anthony Todd <toddy201@gmail.com>
2014-05-12 14:53:39 +0100

    Ball bounces off the camera edge, Added animated blocks

--- Assets/Scrips/Ball_Controller.cs
+++ Assets/Scrips/Ball_Controller.cs
@@ -1,15 +1,63 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
+[RequireComponent ( typeof ( Rigidbody2D ), typeof ( CircleCollider2D ) )]
 public class Ball_Controller : MonoBehaviour {
+
+    CircleCollider2D _circleCollider;
+    Rigidbody2D _rigidbody2D;
+    Camera _mainCamera;
+    Vector3 _screenWidth = new Vector3 ();
+    Vector2 _vel = new Vector2();
+
+    public float speed = 2;
+
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D> ();
+        _circleCollider = collider2D as CircleCollider2D;
+        _mainCamera = Camera.main;
+    }
 
 	// Use this for initialization
 	void Start () {
-	
+        //rigidbody2D.velocity = UnityEngine.Random.insideUnitCircle.normalized * speed;
+        rigidbody2D.velocity = Vector3.up * speed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+
+    void FixedUpdate()
+    {
+        _screenWidth.Set ( Screen.width, Screen.height, 0 );
+
+        Vector3 cameraWorldMax = _mainCamera.ScreenToWorldPoint ( _screenWidth );
+        Vector3 cameraWorldMin = _mainCamera.ScreenToWorldPoint ( Vector3.zero );
+
+        float minSpeed = speed*speed;
+
+        float rad = _circleCollider.radius;
+        _vel = _rigidbody2D.velocity;
+        if( (transform.position.x - rad) < cameraWorldMin.x || (transform.position.x + rad) > cameraWorldMax.x )
+        {
+            _vel.x *= -1;
+        }
+
+        if ( ( transform.position.y - rad ) < cameraWorldMin.y || (transform.position.y + rad ) > cameraWorldMax.y )
+        {
+            _vel.y *= -1;
+        }
+
+        if( _vel.sqrMagnitude < minSpeed )
+        {
+            _vel += _vel * ( minSpeed - _vel.sqrMagnitude );
+        }
+
+        _rigidbody2D.velocity = _vel;
+    }
 }
--- Assets/Scrips/Brick_Controller.cs
+++ Assets/Scrips/Brick_Controller.cs
@@ -1,15 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class Brick_Controller : MonoBehaviour {
+
+    public bool Imortal = false;
+    public int Life = 1;
+
+    public Sprite[] SpriteSequence;
+    public Sprite   SpriteImortal;
+
+    public AnimationCurve ImpactAnimation;
+
+
+
+    SpriteRenderer  spriteRenderer;
+
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer> ();
+
+        Life = Mathf.Clamp ( Life, 0, (SpriteSequence.Length - 1) );
+    }
 
 	// Use this for initialization
 	void Start () {
 	
+        if(Imortal)
+        {
+            spriteRenderer.sprite = SpriteImortal;
+        }
+        else
+        {
+            spriteRenderer.sprite = SpriteSequence [Life];
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+    IEnumerator ImpactAnimationCoroutine ( Vector2 normal )
+    {
+        yield return new WaitForSeconds(0.1f);
+        float t = Time.deltaTime;
+        Vector3 offset = new Vector3 ();
+        Vector3 position = transform.position;
+
+        while( t < 1.0f)
+        {
+            float dist = ImpactAnimation.Evaluate ( t ) * 0.1f;
+            offset = normal * dist;
+
+            transform.localPosition = position + offset;
+            yield return null;
+            t += Time.deltaTime;
+        }
+
+
+        yield return null;
+    }
+
+    void OnCollisionExit2D ( Collision2D coll )
+    {
+        if( !Imortal )
+        {
+            StopCoroutine ( "ImpactAnimationCoroutine" );
+            StartCoroutine ( ImpactAnimationCoroutine ( coll.contacts [0].normal ) );
+            Life--;
+            if ( Life < 0 )
+            {
+                Destroy ( gameObject );
+            }
+            else
+            {
+                spriteRenderer.sprite = SpriteSequence [Life];
+            }
+        }
+    }
 }
