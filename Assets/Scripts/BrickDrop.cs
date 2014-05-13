d4550015bb07fd044ad6e7417246961eb45e4d26
Anthony Todd <toddy201@gmail.com>
2014-05-13 22:18:53 +0100

    Added Power Drop prefab and stub for the script updated paddle to work with ball pool.

---
+++ Assets/Scripts/BrickDrop.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Darkhexxa.SimplePool.Components;
+
+[RequireComponent(typeof(Rigidbody2D),typeof(CircleCollider2D))]
+public class BrickDrop : BasePoolComponent {
+
+    public   float DropGravity = 2;
+    private  Camera _mainCamera;
+    private  CircleCollider2D _circleCollider;
+
+    void Awake()
+    {
+        rigidbody2D.gravityScale = DropGravity;
+        _mainCamera = Camera.main;
+
+        _circleCollider = collider2D as CircleCollider2D;
+    }
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        Vector3 cameraWorldMin = _mainCamera.ScreenToWorldPoint ( Vector3.zero );
+
+        float rad = _circleCollider.radius;
+
+        if ( ( transform.position.y + rad ) < cameraWorldMin.y )
+        {
+            pool.Despawn ( gameObject );
+        }
+    }
+
+    void OnTriggerEnter2D ( Collider2D col )
+    {
+        Debug.Log ( "Triggered" );
+    }
+
+    public override void OnSpawn ()
+    {
+    }
+
+    public override void OnDespawn ()
+    {
+    }
+
+
+}
--- Assets/Scripts/Paddle_Controller.cs
+++ Assets/Scripts/Paddle_Controller.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Paddle_Controller : MonoBehaviour {
 
+    public float speed = 4.0f;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,6 @@ public class Paddle_Controller : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        rigidbody2D.velocity = Input.GetAxis ( "Horizontal" ) * Vector2.right * speed;
 	}
 }
